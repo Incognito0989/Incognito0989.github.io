253e76a4c22f5c1c39202146ef10483ec0ee38ef
andrew.jones <andrew109jones@gmail.com>
2023-01-27 02:12:24 -0500

    add envelope

--- index.js
+++ index.js
@@ -309,3 +309,61 @@ window.addEventListener('keyup', ({keyCode}) => {
             break
     }
 })
+
+var envelope = document.getElementById('envelope')
+var envelopeTop = document.getElementById('envelopeTop')
+var letter = document.getElementById('contact')
+var left = document.getElementById('left')
+var bottomRight = document.getElementById('bottome-right')
+var flag = 1;
+
+'use strict';
+(function() {
+    envelopeTop.classList.add('close')
+    envelope.addEventListener('mouseover', openEnvelopeOnHover)
+    envelope.addEventListener('mouseout', closeEnvelopeOnHover)
+    envelope.addEventListener('click', openEnvelope)
+})();
+
+function openEnvelopeOnHover() {
+    envelopeTop.classList.remove('close')
+    envelopeTop.classList.add('open')
+    pullOutPartial()
+}
+function closeEnvelopeOnHover() {
+    if(flag === 1) {
+        putIn()
+        envelopeTop.classList.remove('open')
+        envelopeTop.classList.add('close')
+    } else {
+        envelope.removeEventListener('mouseout', closeEnvelopeHover)
+    }
+}
+function pullOutPartial() {
+    letter.classList.remove('in')
+    letter.classList.add('out-partial')
+}
+function putIn() {
+    letter.classList.remove('out-partial')
+    left.classList.add('in')
+}
+function openEnvelope() {
+    flag = 0;
+    closeEnvelopeHover();
+    letter.classList.add('pull')
+    letter.addEventListener('animationend', function() {
+        left.style.zIndex = 0
+        bottomRight.style.zIndex = 0
+        envelopeTop.style.zIndex = 0
+        letter.style.zIndex = 20
+        letter.classList.add('put')
+        letter.addEventListener('animationend', function() {
+            letter.style.transition = 'none'
+            letter.classList.add('final')
+            letter.classList.remove('put')
+            letter.classList.remove('pull')
+            envelope.style.cursor = 'default'
+        })
+    })
+    envelope.removeEventListener('click', openEnvelope)
+}
